9fb7c94d259a623a5cad36f8ccb6f8a49f21cf52
Sam Moore <matches@ucc.asn.au>
2014-04-11 17:31:41 +0800

    Remove REAL_HALF as it doesn't actually implement a half precision float
    More to it than just masking bits because the sizes of the exponent differ.
    Could just mask bits in the mantissa but I have horrible plans involving VFPU...

--- src/tests/vfpufloat.cpp
+++ src/tests/vfpufloat.cpp
@@ -6,7 +6,10 @@ using namespace std;
 
 int main(int argc, char ** argv)
 {
-	VFPU::Start();
+	if (argc > 1)
+		VFPU::Start(argv[1]);
+	else
+		VFPU::Start();
 	float result = VFPU::Exec(25,10, VFPU::SUB);
 	Debug("%f\n", result);
 	VFPU::Halt();
